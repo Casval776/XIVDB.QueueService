9585316620c088e5bfd8a549faae36906b91642b
Steven Grant <sgrant@campsystems.com>
2016-06-14 15:15:29 -0400

    Initial push for Queue Service for Database Layer and Spider Layer. Logging not yet enabled. Basic functionality enabled. Published in IIS.

---
+++ Queue/Queue/Queue.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Queue
+{
+    public class Queue
+    {
+        private Queue<string> ItemQueue;
+        private static Queue instance;
+
+        private Queue()
+        {
+            
+            ItemQueue = new Queue<string>();
+        }
+
+        static Queue()
+        {
+
+        }
+
+        public static Queue Instance
+        {
+            get
+            {
+                if (instance == null) instance = new Queue();
+                return instance;
+            }
+        }
+        public string Dequeue()
+        {
+            try
+            {
+                return Instance.ItemQueue.Dequeue();
+            }
+            catch(Exception e)
+            {
+                return string.Empty;
+            }
+        }
+
+        public bool Enqueue(string item)
+        {
+            bool Success = false;
+            try
+            {
+                Instance.ItemQueue.Enqueue(item);
+                Success = true;
+            }
+            catch (Exception ex)
+            {
+                //@TODO: Add Logging here
+                return Success;
+            }
+            return Success;
+        }
+
+        public IEnumerable<string> ViewQueue()
+        {
+            List<string> itemList = new List<string>();
+            Parallel.ForEach(ItemQueue, (item) =>
+            {
+                itemList.Add(item);
+            });
+            //foreach (var item in ItemQueue)
+            //{
+            //    itemList.Add(item);
+            //}
+            return itemList;
+        }
+    }
+}
---
+++ Queue/Queue/XIVDB.QueueService.svc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class QueueService : IQueueService
+    {
+        private Queue thisQueue = Queue.Instance;
+        public string Dequeue()
+        {
+            return thisQueue.Dequeue();
+        }
+
+        public bool Enqueue(string item)
+        {
+            return thisQueue.Enqueue(item);
+        }
+
+        public IEnumerable<string> ViewQueue()
+        {
+            return thisQueue.ViewQueue();
+        }
+    }
+}
